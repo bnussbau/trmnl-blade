4913495d574cc66e8d22ce6eb8e8ac59ac9d77a3
Benjamin Nussbaum <bnussbau@users.noreply.github.com>
2026-04-03 08:32:21 +0200

    feat: bump TRMNL_BLADE_FRAMEWORK_CSS_VERSION to 2.3.7

--- config/trmnl-blade.php
+++ config/trmnl-blade.php
@@ -2,9 +2,9 @@
 
 // config for Bnussbau/TrmnlBlade
 return [
-    'framework_version' => env('TRMNL_BLADE_FRAMEWORK_VERSION', '2.1.0'),
-    'framework_css_version' => env('TRMNL_BLADE_FRAMEWORK_CSS_VERSION', '2.3.3'),
-    'framework_js_version' => env('TRMNL_BLADE_FRAMEWORK_JS_VERSION', '2.1.0'),
+    'framework_version' => env('TRMNL_BLADE_FRAMEWORK_VERSION', '2.3.7'),
+    'framework_css_version' => env('TRMNL_BLADE_FRAMEWORK_CSS_VERSION', null),
+    'framework_js_version' => env('TRMNL_BLADE_FRAMEWORK_JS_VERSION', null),
     'framework_css_url' => env('TRMNL_BLADE_FRAMEWORK_CSS_URL', null),
     'framework_js_url' => env('TRMNL_BLADE_FRAMEWORK_JS_URL', null),
 ];
